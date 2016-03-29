06412db82cc3aa701d742cad593881985cb1e433
dananggeek <dananggeek@gmail.com>
2016-03-29 14:22:12 +0700

    admin

---
+++ datasiswa.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+ 
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="../desain/css/bootstrap.css">
+  	<link rel="stylesheet" href="../desain/css/custom.css">
+  	<link rel="stylesheet" href="../desain/js/bootstrap.js">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="desain/js/jquery-1.11.0.min.js"></script>
+    <script src="desain/js/bootstrap.min.js"></script>
+
+
+
+
+	
+</style>
+<body>
+	<?php
+		include "../menu/menuadmin.php"
+	?>
+	
+	
+	<div class="container-fluid">
+		<div class="table-responsive table-striped">
+			<table class="table"
+				<caption> <h3>  Data Pendaftaran Siswa Secara Online </h3> </caption>
+				<thead>
+					<tr>
+						<th>Nama Lengkap </th>
+						<th>Alamat </th>
+						<th>Asal Sekolah </th>
+						<th>Nilai Test Online </th>
+						<th>Jurusan </th>
+						<th rowspan="3" class="text-center">Action </th>
+						
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Panggil dengan database	</td>
+						<td>Panggil dengan database	</td>
+						<td>Panggil dengan database	</td>
+						<td>Panggil dengan database	</td>
+						<td>Pamggil Jurusan dr database</th>
+						<td>Hapus 	</td>
+						<td>Edit	</td>
+						<td>Lihat Data Lengkap	</td>
+					</tr>
+					
+					<tr>
+						<td>Panggil dengan database	</td>
+						<td>Panggil dengan database	</td>
+						<td>Panggil dengan database	</td>
+						<td>Panggil dengan database	</td>
+						<td>Pamggil Jurusan dr database</th>
+						<td>Hapus 	</td>
+						<td>Edit	</td>
+						<td>Lihat Data Lengkap	</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	
+	<?php
+		include "../menu/footerindex.php"
+	?>
+</body> 
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+ 
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="../desain/css/bootstrap.css">
+  	<link rel="stylesheet" href="../desain/css/custom.css">
+  	<link rel="stylesheet" href="../desain/js/bootstrap.js">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="desain/js/jquery-1.11.0.min.js"></script>
+    <script src="desain/js/bootstrap.min.js"></script>
+
+
+
+
+	
+</style>
+<body>
+	<?php
+		include "../menu/menuadmin.php"
+	?>
+
+		
+		
+	
+	</div>
+	
+	<div class="container-fluid">
+	
+	</div>
+	<?php
+		include "../menu/footerindex.php"
+	?>
+</body> 
+</html>
